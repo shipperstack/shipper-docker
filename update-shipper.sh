8debb9b70d833be1dc976fba3e65b268e0519a87
Eric Park <me@ericswpark.com>
2022-05-21 16:16:37 +0900

    Add script to update shipper

---
+++ update-shipper.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Before running this file, fetch changes using git!
+
+# Source activate alias file
+. ./activate
+
+# Set latest version tag
+setlatest
+
+# Start Docker images
+docker-compose up -d --no-build
+
+# Migrate database, compile translations, and collect static files
+./django-update.sh
+
